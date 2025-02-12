77ecfcbfa3e604d2d6237c788671383e6b1b8eee
sohilaakram <sohilaakram305@gmail.com>
2025-02-12 20:59:50 +0200

    implement logic to toggle sidebar

--- src/app/shop/sidebar/sidebar.component.ts
+++ src/app/shop/sidebar/sidebar.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  @Output() closeEvent = new EventEmitter();
 
+  closeMenu() {
+    this.closeEvent.emit(); // Notify parent to close the sidebar
+  }
 }
